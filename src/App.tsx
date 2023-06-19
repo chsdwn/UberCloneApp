9ad82cdc8fcb6aae590e16d4b4bd82593f1c2de7
chsdwn <chasedown@hotmail.com>
2023-06-19 15:52:30 +0300

    build: install react-native-elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { SafeAreaView, StatusBar, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
 
@@ -7,17 +8,19 @@ import { store } from './store';
 
 export const App = () => {
   return (
-    <NavigationContainer>
-      <Provider store={store}>
-        <SafeAreaView className="flex-1 bg-gray-300">
-          <StatusBar backgroundColor="white" barStyle="dark-content" />
-          <View className="flex-1">
-            <Text className="font-bold text-red-500 text-center">
-              Uber Clone
-            </Text>
-          </View>
-        </SafeAreaView>
-      </Provider>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Provider store={store}>
+          <SafeAreaView className="flex-1 bg-gray-300">
+            <StatusBar backgroundColor="white" barStyle="dark-content" />
+            <View className="flex-1">
+              <Text className="font-bold text-red-500 text-center">
+                Uber Clone
+              </Text>
+            </View>
+          </SafeAreaView>
+        </Provider>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 };
