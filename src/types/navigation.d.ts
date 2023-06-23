b06d4adb903dd0068a7588025da7634ec3da183f
chsdwn <chasedown@hotmail.com>
2023-06-23 15:21:08 +0300

    feat(types): create navigation types

---
+++ src/types/navigation.d.ts
@@ -0,0 +1,8 @@
+import { StackNavigationProp } from '@react-navigation/stack';
+
+export type HomeStackParamList = {
+  Navigation: undefined;
+  Map: undefined;
+};
+
+export type HomeStackNavigationProp = StackNavigationProp<HomeStackParamList>;
